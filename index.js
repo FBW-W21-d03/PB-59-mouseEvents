c533e0e93852ac2abbc7a4a42b2b28b43708ec80
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-18 15:20:39 +0100

    mein projekt

---
+++ index.js
@@ -0,0 +1,385 @@
+// PRODUCT 1
+
+function diesel_kaufen() {
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro1").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro1").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Diesel Chronograph :399.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function diesel_pic2() {
+  document.getElementById("diesel_uhr").src =
+    "diesel-chronograph-mega-chief-dz4582_88381408.jpg";
+    document.getElementById("diesel_uhr").style.width = "358px";
+    document.getElementById("diesel_uhr").style.height = "358px";
+}
+
+function diesel_pic1() {
+  document.getElementById("diesel_uhr").src =
+    "diesel-chronograph-mega-chief-dz4582_88381408_m.jpg" ;
+   document.getElementById("diesel_uhr").style.width = "500px";
+   document.getElementById("diesel_uhr").style.height = "500px";
+}
+
+// PRODUCT 2
+
+function boss_kaufen() {
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro2").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro2").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li> Hugo Boss :499.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function boss_pic2() {
+    document.getElementById("boss_uhr").src =
+    "hugo-boss-chronograph-1513755_87887189.jpg";
+    document.getElementById("boss_uhr").style.width = "358px";
+    document.getElementById("boss_uhr").style.height = "358px";
+}
+
+function boss_pic1() {
+  document.getElementById("boss_uhr").src =
+    "hugo-boss-chronograph-1513755_87887189_1.jpg";
+    document.getElementById("boss_uhr").style.width = "500px";
+    document.getElementById("boss_uhr").style.height = "500px";
+}
+
+// PRODUCT 3
+
+function jette_kaufen() {
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro3").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro3").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Jette Damenuhr :299.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function jette_pic2() {
+    document.getElementById("jette_uhr").src =
+    "jette-damenuhr-88339967_88339967.jpg";
+    document.getElementById("jette_uhr").style.width = "358px";
+    document.getElementById("jette_uhr").style.height = "358px";
+
+}
+
+function jette_pic1() {
+  document.getElementById("jette_uhr").src =
+    "jette-damenuhr-88339967_88339967_m.jpg";
+    document.getElementById("jette_uhr").style.width = "500px";
+    document.getElementById("jette_uhr").style.height = "500px";
+}
+
+// PRODUCT 4
+
+function maserati_kaufen() {
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro4").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro4").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li> Maserati :499.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function maserati_pic2() {
+  document.getElementById("maserati_uhr").src =
+    "maserati-chronograph-r8873640008_88183991.jpg";
+    document.getElementById("maserati_uhr").style.width = "358px";
+    document.getElementById("maserati_uhr").style.height = "358px";
+}
+
+function maserati_pic1() {
+  document.getElementById("maserati_uhr").src =
+    "maserati-chronograph-r8873640008_88183991_5.jpg";
+    document.getElementById("maserati_uhr").style.width = "500px";
+    document.getElementById("maserati_uhr").style.height = "500px";
+}
+
+// PRODUCT 5
+
+function longines_kaufen() {
+
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro5").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro5").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Longines Herrenuhr  :3299.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function longines_pic2() {
+  document.getElementById("longines_uhr").src =
+    "longines-herrenuhr-heritage-avigation-l28161932_88224523.jpg";
+    document.getElementById("longines_uhr").style.width = "358px";
+    document.getElementById("longines_uhr").style.height = "358px";
+}
+
+function longines_pic1() {
+  document.getElementById("longines_uhr").src =
+    "longines-herrenuhr-heritage-avigation-l28161932_88224523_3.jpg";
+    document.getElementById("longines_uhr").style.width = "500px";
+    document.getElementById("longines_uhr").style.height = "500px";
+}
+
+// PRODUCT 6
+
+function flik_kaufen() {
+
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro6").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro6").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Flik Flak Kinderuhr :69.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function flik_pic2() {
+  document.getElementById("flik_uhr").src =
+    "flik-flak-kinderuhr-fcsp107_88227468.jpg";
+    document.getElementById("flik_uhr").style.width = "358px";
+    document.getElementById("flik_uhr").style.height = "358px";
+}
+
+function flik_pic1() {
+  document.getElementById("flik_uhr").src =
+    "flik-flak-kinderuhr-fcsp107_88227468_2.jpg";
+    document.getElementById("flik_uhr").style.width = "500px";
+    document.getElementById("flik_uhr").style.height = "500px";
+}
+
+// PRODUCT 7
+
+function maurice_kaufen() {
+
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro7").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro7").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Maurice Lacroix :2499.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function maurice_pic2() {
+  document.getElementById("maurice_uhr").src =
+    "maurice-lacroix-herrenuhr-aikon-venture-gmt-ai6158-ss002-130-1_88229606.jpg";
+    document.getElementById("maurice_uhr").style.width = "358px";
+    document.getElementById("maurice_uhr").style.height = "358px";
+}
+
+function maurice_pic1() {
+  document.getElementById("maurice_uhr").src =
+    "maurice-lacroix-herrenuhr-aikon-venture-gmt-ai6158-ss002-130-1_88229606_2.jpg";
+    document.getElementById("maurice_uhr").style.width = "500px";
+    document.getElementById("maurice_uhr").style.height = "500px";
+}
+
+// PRODUCT 8
+
+function timberland_kaufen() {
+
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro8").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro8").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Timberland Herrenuhr :299.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function timberland_pic2() {
+  document.getElementById("timberland_uhr").src =
+    "timberland-herrenuhr-tdwga2101603_88253833.jpg";
+    document.getElementById("timberland_uhr").style.width = "358px";
+    document.getElementById("timberland_uhr").style.height = "358px";
+}
+
+function timberland_pic1() {
+  document.getElementById("timberland_uhr").src =
+    "timberland-herrenuhr-tdwga2101603_88253833_3.jpg";
+    document.getElementById("timberland_uhr").style.width = "500px";
+    document.getElementById("timberland_uhr").style.height = "500px";
+}
+
+// PRODUCT 9
+
+function movado_kaufen() {
+
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro9").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro9").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> MOVADO Herrenuhr :999.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function movado_pic2() {
+  document.getElementById("movado_uhr").src =
+    "movado-herrenuhr-museum-classic-0607201_88325389.jpg";
+    document.getElementById("movado_uhr").style.width = "358px";
+    document.getElementById("movado_uhr").style.height = "358px";
+}
+
+function movado_pic1() {
+  document.getElementById("movado_uhr").src =
+    "movado-herrenuhr-museum-classic-0607201_88325389_1.jpg";
+    document.getElementById("movado_uhr").style.width = "500px";
+    document.getElementById("movado_uhr").style.height = "500px";
+}
+
+// PRODUCT 10
+
+function tommy_kaufen() {
+
+  // TOTAL PRISE ABOVE
+
+  document.getElementById("total").innerHTML =
+    Number(document.getElementById("total").innerHTML) +
+    Number(document.getElementById("euro10").innerHTML);
+
+  //TOTAL PRISE BOTTOM
+
+  document.getElementById("total2").innerHTML =
+    Number(document.getElementById("total2").innerHTML) +
+    Number(document.getElementById("euro10").innerHTML);
+
+  // LIST
+
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li> Tommy Hilfiger Damenuhr :199.00€</li>";
+}
+
+// BILDER FUNCTION
+
+function tommy_pic2() {
+  document.getElementById("tommy_uhr").src =
+    "tommy-hilfiger-damenuhr-1782382_88342062.jpg";
+    document.getElementById("tommy_uhr").style.width = "358px";
+    document.getElementById("tommy_uhr").style.height = "358px";
+}
+
+function tommy_pic1() {
+  document.getElementById("tommy_uhr").src =
+    "tommy-hilfiger-damenuhr-1782382_88342062_2.jpg";
+    document.getElementById("tommy_uhr").style.width = "500px";
+    document.getElementById("tommy_uhr").style.height = "500px";
+}
+
