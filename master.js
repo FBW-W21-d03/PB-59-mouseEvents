4fb40b37f1cd3fa6489a74cc05e573b7ce35b109
jaber-1988 <jaber1988.1988@gmail.com>
2022-01-18 15:00:30 +0100

    jaber

---
+++ master.js
@@ -0,0 +1,72 @@
+
+
+function nilpferd1() {
+    document.getElementById("Tux1").style.width="500px";
+};
+
+function pinquin1() {
+    document.getElementById("Tux1") .style.width="100px";
+};
+function nilpferd2() {
+    document.getElementById("Tux2").style.width="500px";
+};
+
+function pinquin2() {
+    document.getElementById("Tux2") .style.width="100px";
+};
+function nilpferd3() {
+    document.getElementById("Tux3").style.width="500px";
+};
+
+function pinquin3() {
+    document.getElementById("Tux3") .style.width="100px";
+};
+function nilpferd4() {
+    document.getElementById("Tux4").style.width="500px";
+};
+
+function pinquin4() {
+    document.getElementById("Tux4") .style.width="100px";
+};
+function nilpferd5() {
+    document.getElementById("Tux5").style.width="500px";
+};
+
+function pinquin5() {
+    document.getElementById("Tux5") .style.width="100px";
+};
+function nilpferd6() {
+    document.getElementById("Tux6").style.width="500px";
+};
+
+function pinquin6() {
+    document.getElementById("Tux7") .style.width="100px";
+};
+function nilpferd7() {
+    document.getElementById("Tux8").style.width="500px";
+};
+
+function pinquin7() {
+    document.getElementById("Tux8") .style.width="100px";
+};
+function nilpferd8() {
+    document.getElementById("Tux9").style.width="500px";
+};
+
+function pinquin8() {
+    document.getElementById("Tux9") .style.width="100px";
+};
+function nilpferd9() {
+    document.getElementById("Tux10").style.width="500px";
+};
+
+function pinquin9() {
+    document.getElementById("Tux10") .style.width="100px";
+};
+function nilpferd10() {
+    document.getElementById("Tux10").style.width="500px";
+};
+
+function pinquin10() {
+    document.getElementById("Tux10") .style.width="100px";
+};
