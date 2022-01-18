6f92f7ee02af305f3678f0579443a473b0df3fb5
slavablack <veaceslav.ciornii@magenta.de>
2022-01-18 14:41:55 +0100

    raw version

---
+++ index.js
@@ -0,0 +1,128 @@
+let summe = 0;
+
+function buyFunction(price) {
+document.getElementById("footer-list").innerHTML=
+ document.getElementById("footer-list").innerHTML+ "<li>" + price + "</li>";
+ summe = summe + price;
+ document.getElementById("footer-p").innerHTML="Total: " + summe + " $";
+
+
+     
+};
+
+
+function myFunction () {
+    document.getElementById("briliantwatch").style.width  ="500px";
+    document.getElementById("briliantwatch").style.height = "500px";
+    document.getElementById("briliantwatch").style.cursor = "zoom-in";
+   
+};
+function myFunctionout() {
+    document.getElementById("briliantwatch").style.width  ="300px";
+    document.getElementById("briliantwatch").style.height = "300px";
+};
+
+function myFunction1 () {
+    document.getElementById("briliantwatch1").style.width  ="500px";
+    document.getElementById("briliantwatch1").style.height = "500px";
+    document.getElementById("briliantwatch1").style.cursor = "zoom-in";
+   
+};
+function myFunctionout1() {
+    document.getElementById("briliantwatch1").style.width  ="300px";
+    document.getElementById("briliantwatch1").style.height = "300px";
+};
+
+function myFunction2 () {
+    document.getElementById("briliantwatch2").style.width  ="500px";
+    document.getElementById("briliantwatch2").style.height = "500px";
+    document.getElementById("briliantwatch2").style.cursor = "zoom-in";
+   
+};
+function myFunctionout2() {
+    document.getElementById("briliantwatch2").style.width  ="300px";
+    document.getElementById("briliantwatch2").style.height = "300px";
+};
+
+function myFunction3 () {
+    document.getElementById("briliantwatch3").style.width  ="500px";
+    document.getElementById("briliantwatch3").style.height = "500px";
+    document.getElementById("briliantwatch3").style.cursor = "zoom-in";
+   
+};
+function myFunctionout3() {
+    document.getElementById("briliantwatch3").style.width  ="300px";
+    document.getElementById("briliantwatch3").style.height = "300px";
+};
+
+function myFunction4 () {
+    document.getElementById("briliantwatch4").style.width  ="500px";
+    document.getElementById("briliantwatch4").style.height = "500px";
+    document.getElementById("briliantwatch4").style.cursor = "zoom-in";
+   
+};
+function myFunctionout4() {
+    document.getElementById("briliantwatch4").style.width  ="300px";
+    document.getElementById("briliantwatch4").style.height = "300px";
+};
+
+function myFunction5 () {
+    document.getElementById("briliantwatch5").style.width  ="500px";
+    document.getElementById("briliantwatch5").style.height = "500px";
+    document.getElementById("briliantwatch5").style.cursor = "zoom-in";
+   
+};
+function myFunctionout5() {
+    document.getElementById("briliantwatch5").style.width  ="300px";
+    document.getElementById("briliantwatch5").style.height = "300px";
+};
+
+function myFunction6 () {
+    document.getElementById("briliantwatch6").style.width  ="500px";
+    document.getElementById("briliantwatch6").style.height = "500px";
+    document.getElementById("briliantwatch6").style.cursor = "zoom-in";
+   
+};
+function myFunctionout6() {
+    document.getElementById("briliantwatch6").style.width  ="300px";
+    document.getElementById("briliantwatch6").style.height = "300px";
+};
+
+function myFunction7 () {
+    document.getElementById("briliantwatch7").style.width  ="500px";
+    document.getElementById("briliantwatch7").style.height = "500px";
+    document.getElementById("briliantwatch7").style.cursor = "zoom-in";
+   
+};
+function myFunctionout7() {
+    document.getElementById("briliantwatch7").style.width  ="300px";
+    document.getElementById("briliantwatch7").style.height = "300px";
+};
+
+function myFunction8 () {
+    document.getElementById("briliantwatch8").style.width  ="500px";
+    document.getElementById("briliantwatch8").style.height = "500px";
+    document.getElementById("briliantwatch8").style.cursor = "zoom-in";
+   
+};
+function myFunctionout8() {
+    document.getElementById("briliantwatch8").style.width  ="300px";
+    document.getElementById("briliantwatch8").style.height = "300px";
+};
+
+function myFunction9 () {
+    document.getElementById("briliantwatch9").style.width  ="500px";
+    document.getElementById("briliantwatch9").style.height = "500px";
+    document.getElementById("briliantwatch9").style.cursor = "zoom-in";
+   
+};
+function myFunctionout9() {
+    document.getElementById("briliantwatch9").style.width  ="300px";
+    document.getElementById("briliantwatch9").style.height = "300px";
+};
+function newFunction(){
+    document.getElementById("buy").style.backgroundColor="red";
+}
+function newFunctionout(){
+    document.getElementById("buy").style.backgroundColor="gold";
+}
