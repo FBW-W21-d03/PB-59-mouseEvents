f0ebac7b1a1c18741b1f7366120dbb331e70bbd5
Dennisbullys <d.rochus@hotmail.de>
2022-01-18 15:05:06 +0100

    Dennis ist baer noch nicht fertig muss leider zum Termin

---
+++ java.js
@@ -0,0 +1,61 @@
+function zoomIn() {
+  document.getElementById("patek").style.width = "300px";
+  document.getElementById("patek").style.height = "300px";
+}
+
+function zoomOut() {
+  document.getElementById("patek").style.width = "100px";
+  document.getElementById("patek").style.height = "100px";
+}
+
+function zoomIn1() {
+  document.getElementById("hublot").style.width = "300px";
+  document.getElementById("hublot").style.height = "300px";
+}
+
+function zoomOut1() {
+  document.getElementById("hublot").style.width = "100px";
+  document.getElementById("hublot").style.height = "100px";
+}
+
+function zoomIn2() {
+  document.getElementById("audemars").style.width = "300px";
+  document.getElementById("audemars").style.height = "300px";
+}
+
+function zoomOut2() {
+  document.getElementById("audemars").style.width = "100px";
+  document.getElementById("audemars").style.height = "100px";
+}
+function zoomIn3() {
+  document.getElementById("lange").style.width = "300px";
+  document.getElementById("lange").style.height = "300px";
+}
+
+function zoomOut3() {
+  document.getElementById("lange").style.width = "100px";
+  document.getElementById("lange").style.height = "100px";
+}
+function zoomIn4() {
+  document.getElementById("rolex").style.width = "300px";
+  document.getElementById("rolex").style.height = "300px";
+}
+
+function zoomOut4() {
+  document.getElementById("rolex").style.width = "100px";
+  document.getElementById("rolex").style.height = "100px";
+}
+function zoomIn5() {
+  document.getElementById("mille").style.width = "300px";
+  document.getElementById("mille").style.height = "300px";
+}
+
+function zoomOut5() {
+  document.getElementById("mille").style.width = "100px";
+  document.getElementById("mille").style.height = "100px";
+}
+
+function kaufen() {
+  console.log("Im Warenkorb ");
+  alert("Im Warenkorb");
+}
