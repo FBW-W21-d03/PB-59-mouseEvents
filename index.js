9e6362f0a7ecfda272ef3193fa78312ee69b142e
Mak513 <osei.makangu@hotmail.com>
2022-01-18 14:18:59 +0100

    osei

---
+++ index.js
@@ -0,0 +1,85 @@
+
+function kaufen(){
+  console.log("In Warenkorb");
+  alert("In Warenkorb")
+}
+
+//UHR 1
+function watchIn() {
+  document.getElementById("rol1").src = "./images/rol2.webp";
+}
+function watchOut() {
+  document.getElementById("rol1").src = "./images/rol1.webp";
+}
+
+//UHR 2
+function watchIn2() {
+  document.getElementById("rol2").src = "./images/rol6.webp";
+}
+function watchOut2() {
+  document.getElementById("rol2").src = "./images/rol3.webp";
+}
+
+//UHR 3
+function watchIn3() {
+  document.getElementById("rol3").src = "./images/rol8.webp";
+}
+function watchOut3() {
+  document.getElementById("rol3").src = "./images/rol7.webp";
+}
+
+//UHR 4
+function watchIn4() {
+  document.getElementById("rol4").src = "./images/rol10.webp";
+}
+function watchOut4() {
+  document.getElementById("rol4").src = "./images/rol9.webp";
+}
+
+//UHR 5
+function watchIn5() {
+  document.getElementById("rol5").src = "./images/rol12.webp";
+}
+function watchOut5() {
+  document.getElementById("rol5").src = "./images/rol11.webp";
+}
+
+//UHR 6
+function watchIn6() {
+  document.getElementById("rol6").src = "./images/rol14.webp";
+}
+function watchOut6() {
+  document.getElementById("rol6").src = "./images/rol13.webp";
+}
+
+//UHR 7
+function watchIn7() {
+  document.getElementById("rol7").src = "./images/rol16.webp";
+}
+function watchOut7() {
+  document.getElementById("rol7").src = "./images/rol15.webp";
+}
+
+//UHR 8
+function watchIn8() {
+  document.getElementById("ro8").src = "./images/rol18.webp";
+}
+function watchOut8() {
+  document.getElementById("rol8").src = "./images/rol17.webp";
+}
+
+//UHR 9
+function watchIn9() {
+  document.getElementById("rol9").src = "./images/rol20.webp";
+}
+function watchOut9() {
+  document.getElementById("rol9").src = "./images/rol19.webp";
+}
+
+//UHR 10
+function watchIn10() {
+  document.getElementById("rol10").src = "./images/rol22.webp";
+}
+function watchOut10() {
+  document.getElementById("rol10").src = "./images/rol21.webp";
+}
