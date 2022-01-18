3fa0a35d71bdf6cfd7ee050495e5177311d8d13b
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-18 14:21:21 +0100

    soweit erstmal..

---
+++ index.js
@@ -0,0 +1,128 @@
+
+"use strict";
+
+function displayOnScreen() {
+    console.log("Hello World!");
+};
+
+function gross01() {
+    document.getElementById("uhr01").style.width = "500px";
+    document.getElementById("uhr01").style.height = "500px";
+    document.getElementById("uhr01").src = "img/uhr01.jpg";
+}
+function klein01() {
+    document.getElementById("uhr01").style.width = "250px";
+    document.getElementById("uhr01").style.height = "250px";
+    document.getElementById("uhr01").src = "img/uhr01.jpg";
+}
+
+function gross02() {
+    document.getElementById("uhr02").style.width = "500px";
+    document.getElementById("uhr02").style.height = "500px";
+    document.getElementById("uhr02").src = "img/uhr02.jpg";
+}
+
+function klein02() {
+    document.getElementById("uhr02").style.width = "250px";
+    document.getElementById("uhr02").style.height = "250px";
+    document.getElementById("uhr02").src = "img/uhr02.jpg";
+}
+
+function gross03() {
+    document.getElementById("uhr03").style.width = "500px";
+    document.getElementById("uhr03").style.height = "500px";
+    document.getElementById("uhr03").src = "img/uhr03.jpg";
+}
+
+function klein03() {
+    document.getElementById("uhr03").style.width = "250px";
+    document.getElementById("uhr03").style.height = "250px";
+    document.getElementById("uhr03").src = "img/uhr03.jpg";
+}
+
+function gross04() {
+    document.getElementById("uhr04").style.width = "500px";
+    document.getElementById("uhr04").style.height = "500px";
+    document.getElementById("uhr04").src = "img/uhr04.jpg";
+}
+
+function klein04() {
+    document.getElementById("uhr04").style.width = "250px";
+    document.getElementById("uhr04").style.height = "250px";
+    document.getElementById("uhr04").src = "img/uhr04.jpg";
+}
+
+function gross05() {
+    document.getElementById("uhr05").style.width = "500px";
+    document.getElementById("uhr05").style.height = "500px";
+    document.getElementById("uhr05").src = "img/uhr05.jpg";
+}
+
+function klein05() {
+    document.getElementById("uhr05").style.width = "250px";
+    document.getElementById("uhr05").style.height = "250px";
+    document.getElementById("uhr05").src = "img/uhr05.jpg";
+}
+
+function gross06() {
+    document.getElementById("uhr06").style.width = "500px";
+    document.getElementById("uhr06").style.height = "500px";
+    document.getElementById("uhr06").src = "img/uhr06.jpg";
+}
+
+function klein06() {
+    document.getElementById("uhr06").style.width = "250px";
+    document.getElementById("uhr06").style.height = "250px";
+    document.getElementById("uhr06").src = "img/uhr06.jpg";
+}
+
+function gross07() {
+    document.getElementById("uhr07").style.width = "500px";
+    document.getElementById("uhr07").style.height = "500px";
+    document.getElementById("uhr07").src = "img/uhr07.jpg";
+}
+
+function klein07() {
+    document.getElementById("uhr07").style.width = "250px";
+    document.getElementById("uhr07").style.height = "250px";
+    document.getElementById("uhr07").src = "img/uhr07.jpg";
+}
+
+function gross08() {
+    document.getElementById("uhr08").style.width = "500px";
+    document.getElementById("uhr08").style.height = "500px";
+    document.getElementById("uhr08").src = "img/uhr08.jpg";
+}
+
+function klein08() {
+    document.getElementById("uhr08").style.width = "250px";
+    document.getElementById("uhr08").style.height = "250px";
+    document.getElementById("uhr08").src = "img/uhr08.jpg";
+}
+
+function gross09() {
+    document.getElementById("uhr09").style.width = "500px";
+    document.getElementById("uhr09").style.height = "500px";
+    document.getElementById("uhr09").src = "img/uhr09.jpg";
+}
+
+function klein09() {
+    document.getElementById("uhr09").style.width = "250px";
+    document.getElementById("uhr09").style.height = "250px";
+    document.getElementById("uhr09").src = "img/uhr09.jpg";
+}
+
+function gross10() {
+    document.getElementById("uhr10").style.width = "500px";
+    document.getElementById("uhr10").style.height = "500px";
+    document.getElementById("uhr10").src = "img/uhr10.jpg";
+}
+
+function klein10() {
+    document.getElementById("uhr10").style.width = "250px";
+    document.getElementById("uhr10").style.height = "250px";
+    document.getElementById("uhr10").src = "img/uhr10.jpg";
+}
+
+
+
