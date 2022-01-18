e7ac401e75e0741b2607f8a7fc11a5f759c23626
Serdar68 <Serdarbalkis@outlook.de>
2022-01-18 13:18:58 +0100

    serdar

---
+++ java.js
@@ -0,0 +1,114 @@
+function zoomIn() {
+  document.getElementById("Tux").style.width = "200px";
+  document.getElementById("Tux").style.height = "250px";
+}
+
+function zoomOut() {
+  document.getElementById("Tux").style.width = "100px";
+  document.getElementById("Tux").style.height = "150px";
+}
+
+function zoomIn1() {
+  document.getElementById("Tux1").style.width = "200px";
+  document.getElementById("Tux1").style.height = "250px";
+}
+
+function zoomOut1() {
+  document.getElementById("Tux1").style.width = "100px";
+  document.getElementById("Tux1").style.height = "150px";
+}
+
+function zoomIn2() {
+  document.getElementById("Tux2").style.width = "200px";
+  document.getElementById("Tux2").style.height = "250px";
+}
+
+function zoomOut2() {
+  document.getElementById("Tux2").style.width = "100px";
+  document.getElementById("Tux2").style.height = "150px";
+}
+
+function zoomIn3() {
+  document.getElementById("Tux3").style.width = "200px";
+  document.getElementById("Tux3").style.height = "250px";
+}
+
+function zoomOut3() {
+  document.getElementById("Tux3").style.width = "100px";
+  document.getElementById("Tux3").style.height = "150px";
+}
+
+function zoomIn4() {
+  document.getElementById("Tux4").style.width = "200px";
+  document.getElementById("Tux4").style.height = "250px";
+}
+
+function zoomOut4() {
+  document.getElementById("Tux4").style.width = "100px";
+  document.getElementById("Tux4").style.height = "150px";
+}
+
+function zoomIn5() {
+  document.getElementById("Tux5").style.width = "200px";
+  document.getElementById("Tux5").style.height = "250px";
+}
+
+function zoomOut5() {
+  document.getElementById("Tux5").style.width = "100px";
+  document.getElementById("Tux5").style.height = "150px";
+}
+
+function zoomIn6() {
+  document.getElementById("Tux6").style.width = "200px";
+  document.getElementById("Tux6").style.height = "250px";
+}
+
+function zoomOut6() {
+  document.getElementById("Tux6").style.width = "100px";
+  document.getElementById("Tux6").style.height = "150px";
+}
+
+function zoomIn7() {
+  document.getElementById("Tux7").style.width = "200px";
+  document.getElementById("Tux7").style.height = "250px";
+}
+
+function zoomOut7() {
+  document.getElementById("Tux7").style.width = "100px";
+  document.getElementById("Tux7").style.height = "150px";
+}
+
+function zoomIn8() {
+  document.getElementById("Tux8").style.width = "200px";
+  document.getElementById("Tux8").style.height = "250px";
+}
+
+function zoomOut8() {
+  document.getElementById("Tux8").style.width = "100px";
+  document.getElementById("Tux8").style.height = "150px";
+}
+
+function zoomIn9() {
+  document.getElementById("Tux9").style.width = "200px";
+  document.getElementById("Tux9").style.height = "250px";
+}
+
+function zoomOut9() {
+  document.getElementById("Tux9").style.width = "100px";
+  document.getElementById("Tux9").style.height = "150px";
+}
+
+function uhr1() {
+  document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML +
+    "<li>" +
+    "uhr1 - 250" +
+    "</li>";
+
+  document.getElementById("summe").innerHTML =
+    Number(document.getElementById("summe").innerHTML) 
+    +
+    Number(document.getElementById("uhr1").innerHTML);
+
+  console.log("esgeht");
+}
