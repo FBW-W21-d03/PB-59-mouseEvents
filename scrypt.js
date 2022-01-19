3c4c55af53213d0adedbc8a6ce379ad185bac3ae
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-19 09:11:03 +0100

    Orhan's lösung

---
+++ scrypt.js
@@ -0,0 +1,233 @@
+// mediumZoom('.zoom', {
+//     margin: 50
+// })
+
+// mediumZoom(".zoom-dark", {
+//   background: '#000'
+// })
+
+
+// Zoom In Zoom Out function
+
+function zoomIn() {
+  document.getElementById("zoom").style.width = "200px";
+  document.getElementById("zoom").style.height = "auto";
+}
+
+function zoomOut() {
+  document.getElementById("zoom").style.width = "100px";
+  document.getElementById("zoom").style.height = "auto";
+}
+
+function zoomIn1() {
+  document.getElementById("zoom1").style.width = "200px";
+  document.getElementById("zoom1").style.height = "auto";
+}
+
+function zoomOut1() {
+  document.getElementById("zoom1").style.width = "100px";
+  document.getElementById("zoom1").style.height = "auto";
+}
+
+function zoomIn2() {
+  document.getElementById("zoom2").style.width = "200px";
+  document.getElementById("zoom2").style.height = "auto";
+}
+
+function zoomOut2() {
+  document.getElementById("zoom2").style.width = "100px";
+  document.getElementById("zoom2").style.height = "auto";
+}
+
+function zoomIn3() {
+  document.getElementById("zoom3").style.width = "200px";
+  document.getElementById("zoom3").style.height = "auto";
+}
+
+function zoomOut3() {
+  document.getElementById("zoom3").style.width = "100px";
+  document.getElementById("zoom3").style.height = "auto";
+}
+
+function zoomIn4() {
+  document.getElementById("zoom4").style.width = "200px";
+  document.getElementById("zoom4").style.height = "auto";
+}
+
+function zoomOut4() {
+  document.getElementById("zoom4").style.width = "100px";
+  document.getElementById("zoom4").style.height = "auto";
+}
+
+function zoomIn5() {
+  document.getElementById("zoom5").style.width = "200px";
+  document.getElementById("zoom5").style.height = "auto";
+}
+
+function zoomOut5() {
+  document.getElementById("zoom5").style.width = "100px";
+  document.getElementById("zoom5").style.height = "auto";
+}
+
+function zoomIn6() {
+  document.getElementById("zoom6").style.width = "200px";
+  document.getElementById("zoom6").style.height = "auto";
+}
+
+function zoomOut6() {
+  document.getElementById("zoom6").style.width = "100px";
+  document.getElementById("zoom6").style.height = "auto";
+}
+
+function zoomIn7() {
+  document.getElementById("zoom7").style.width = "200px";
+  document.getElementById("zoom7").style.height = "auto";
+}
+
+function zoomOut7() {
+  document.getElementById("zoom7").style.width = "100px";
+  document.getElementById("zoom7").style.height = "auto";
+}
+
+function zoomIn8() {
+  document.getElementById("zoom8").style.width = "200px";
+  document.getElementById("zoom8").style.height = "auto";
+}
+
+function zoomOut8() {
+  document.getElementById("zoom8").style.width = "100px";
+  document.getElementById("zoom8").style.height = "auto";
+}
+
+function zoomIn9() {
+  document.getElementById("zoom9").style.width = "200px";
+  document.getElementById("zoom9").style.height = "auto";
+}
+
+function zoomOut9() {
+  document.getElementById("zoom9").style.width = "100px";
+  document.getElementById("zoom9").style.height = "auto";
+}
+
+// Warenkorb function
+
+  function uhr1() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "TISSOT  - 250 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price1").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr2() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "Apple - 450 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price2").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr3() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "MAMBA - 299 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price3").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr4() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "MAMBA - 199 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price4").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr5() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "IWC - 210 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price5").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr6() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "NIXON  - 250 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price6").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr7() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "FOSSIL - 350 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price7").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr8() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "SEIKO - 199 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price8").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr9() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "TIMEX EXPEDITION - 439 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price9").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
+  function uhr10() {
+    document.getElementById("liste").innerHTML =
+      document.getElementById("liste").innerHTML +
+      "<li>" +
+      "FOSSIL - 329 €" +
+      "</li>";
+    // gesamtbetrag + neues Produkt
+    document.getElementById("sum").innerHTML =
+      Number(document.getElementById("price10").innerHTML) +
+      Number(document.getElementById("sum").innerHTML);
+  }
+
